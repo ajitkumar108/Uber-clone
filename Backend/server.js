3cbbb47fb87a5da9c5107efba4abfa96e610a36a
ajitkumar108 <ajitmishra1729@gmail.com>
2025-02-06 10:07:46 +0530

    Your commit message

--- Backend/server.js
+++ Backend/server.js
@@ -2,8 +2,11 @@ const http=require("http");
 const app=require("./app");
 const port=process.env.PORT ||3000;
 
+const connectDB=require("./db/db");
 const server=http.createServer(app);
 
+
+connectDB();
 app.listen(port , ()=>{
     console.log(`Server is running on port: ${port}`);
 });
---
+++ Frontend/vite.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'vite'
+export default defineConfig({
+  css: {
+    postcss: './postcss.config.js',  // Point to the PostCSS config file
+  },
+})
